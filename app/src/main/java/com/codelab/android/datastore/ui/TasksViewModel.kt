65d3ab493fdce8bee18487fff49f281c51bf38f4
Radek Vala <radek.www@gmail.com>
2020-12-04 09:29:57 +0100

    Initial commit

--- app/src/main/java/com/codelab/android/datastore/ui/TasksViewModel.kt
+++ app/src/main/java/com/codelab/android/datastore/ui/TasksViewModel.kt
@@ -59,6 +59,7 @@ class TasksViewModel(
     }
     val tasksUiModel = tasksUiModelFlow.asLiveData()
 
+
     private fun filterSortTasks(
         tasks: List<Task>,
         showCompleted: Boolean,
